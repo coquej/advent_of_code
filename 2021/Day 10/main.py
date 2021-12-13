8ad64c14060e6615c5b4def8b8c81d2f6ce10a4a
coque <rjardon.ch@gmail.com>
2021-12-13 21:38:41 +0100

    Day 10 completed

--- 2021/Day 10/main.py
+++ 2021/Day 10/main.py
@@ -1,18 +1,19 @@
-
+# Constants
 close_chunk = {"[":"]", "{":"}", "(":")", "<":">" }
 negative_points = {"]":57, "}":1197, ")":3, ">":25137}
 score_points = {"]":2, "}":3, ")":1, ">":4}
     
+    
 def is_corrupted(line):
-    list_opens = []
+    list_open_chunks = []
     
     for chunk in line:
         if chunk in close_chunk.keys():
-            list_opens.append(chunk)
-        elif close_chunk[list_opens[-1]]!=chunk:
+            list_open_chunks.append(chunk)
+        elif close_chunk[list_open_chunks[-1]]!=chunk:
             return True, chunk
         else:
-            list_opens.pop()
+            list_open_chunks.pop()
             
     return False, None
 
@@ -21,14 +22,15 @@ def get_points_from_corrupted(lines):
     for line in lines:
         line_is_corrupted, chunk_received = is_corrupted(line)
         if line_is_corrupted:
-            points+=negative_points[chunk_received]
+            points += negative_points[chunk_received]
     
-    print("Total syntax error score:", points)
+    print("Total syntax error score (part 1):", points)
 
 def get_score_from_line(missing_chunks):
     score = 0
+    
     for chunk in missing_chunks:
-        score=(score*5+score_points[chunk])
+        score=(score*5 + score_points[chunk])
     
     return score
 
@@ -37,19 +39,20 @@ def get_middle_score(lines):
     points = []
     
     for line in cleaned_lines:
-        list_opens = []
+        list_open_chunks = []
         for chunk in line:
             if chunk in close_chunk.keys():
-                list_opens.append(chunk)
+                list_open_chunks.append(chunk)
             else:
-                _ = list_opens.pop()
-        list_opens.reverse()
-        missing_chunks = [close_chunk[char] for char in list_opens]
-        points+=[get_score_from_line(missing_chunks)]
+                _ = list_open_chunks.pop()
+        list_open_chunks.reverse()
+        missing_chunks = [close_chunk[char] for char in list_open_chunks]
+        points += [get_score_from_line(missing_chunks)]
     
     points.sort()
-    print("Middle score:", points[int(len(points)/2)])
-                                        
+    
+    print("Middle score (part 2):", points[int(len(points)/2)])
+    
 
 if __name__ == '__main__':
     lines = [line for line in open("2021/Day 10/input.txt", "r").read().split("\n")]
